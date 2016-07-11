5a0a9a6af3b7a79f22436f7e92d90aab48ec7439
Pierre-LoanJ <loan.janczak@gmail.com>
2016-07-11 17:53:33 +0200

    Update ResizingArrayQueueOfChars.java

--- ResizingArrayQueueOfChars.java
+++ ResizingArrayQueueOfChars.java
@@ -1,19 +1,19 @@
 public class ResizingArrayQueueOfChars {
-  //private int first = 0;
+  private int first = 0;
   private int last  = 0;
   public ResizingArrayQueueOfChars(int n) {
     char[] a = new [n]; 
   }
   public static void isEmpty() {
-    return a[0] == null;
+    return a[first] == null;
   }
   public void enQueue(char c) {
-    t[last] = c;
-    //first++;
+    a[last] = c;
     last++;
   }
   public char deQueue() {
-    char car = t[last];
-    last--;
+    char car = a[first];
+    first++;
+   return car;
   }
 }
