7b0603124b723b7e7eeac367d5880c2dfc03dee5
Pierre-LoanJ <loan.janczak@gmail.com>
2016-07-11 17:24:17 +0200

    Update and rename ResizingArrayQueueOfChar.java to ResizingArrayQueueOfChars.java

---
+++ ResizingArrayQueueOfChars.java
@@ -0,0 +1,16 @@
+public class ResizingArrayQueueOfChars {
+  private int first = 0;
+  private int last  = 0;
+  public ResizingArrayQueueOfChars(int n) {
+    char[] a = new [n]; 
+  }
+  public static void isEmpty() {
+    return a[0] == null;
+  }
+  public void enQueue(char c) {
+    
+  }
+  public char deQueue() {
+    
+  }
+}
