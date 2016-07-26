c3d81ca42afc02abf5915441ae399d785dfa452f
Pierre-LoanJ <loan.janczak@gmail.com>
2016-07-26 20:43:11 +0200

    Update BinarySearchTree.java

--- BinarySearchTree.java
+++ BinarySearchTree.java
@@ -1,1 +1,26 @@
-
+public class BinarySearchTree {
+  private Node root;
+  
+  public class Node {
+    private int val;
+    private int key;
+    private Node left;
+    private Node right;
+    
+    public Node(int key, int val) {
+      this.val = val;
+      this.key = key;
+    }
+  }
+  public void put(int key, int val) {
+    if (this.root == null) {
+      this.root = new Node(key, val);
+    }
+  }
+  public int get(int key) {
+    
+  }
+  public static void main(String[] args) {
+    BinarySearchTree t = new BinarySearchTree();
+  }
+}
